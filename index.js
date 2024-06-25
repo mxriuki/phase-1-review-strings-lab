fcb9c0afe06e19d481696d2c8f4a835c3414957c
mxriuki <mwendav37@gmail.com>
2024-06-25 10:05:56 +0300

    strings

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'Barack Pilly';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+const excitedWelcomeMessage = welcomeMessage.toLocaleUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`
